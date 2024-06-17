f55c7426af48264ebcef5219238f1dd6124bedb1
ibrahem1604h <ibrahim28283@gmail.com>
2024-06-17 04:08:32 +0400

    Create password code.py

---
+++ password code.py
@@ -0,0 +1,15 @@
+import random
+import string
+lenth = int(input("Enter the length of the password: "))
+leters=int(input("Enter the number of leters: "))
+numbers=int(input("Enter the number of numbers: "))
+symbols=int(input("Enter the number of symbols: "))
+total= leters+numbers+symbols
+if total==lenth:
+   char= random.choices(string.ascii_letters, k=leters)
+   num= random.choices(string.digits, k=numbers)
+   sym= random.choices(string.punctuation, k=symbols)
+   pssword=("").join(char+num+sym)
+   print(f"Generated Password: {pssword}")
+else:
+  print("Enter the correct number of characters")
